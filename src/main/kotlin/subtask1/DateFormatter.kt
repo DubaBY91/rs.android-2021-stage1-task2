b706014819a239db5a6578b8e6399494d46fe7be
dubaa <dubaa@mail.ru>
2021-06-17 13:36:40 +0300

    tests 2

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,26 @@
 package subtask1
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+
+        Locale.setDefault(Locale("ru"))
+        var newDateFormat = SimpleDateFormat("d M yyyy")
+        newDateFormat.isLenient = false
+
+        val date: Date? = try {
+            newDateFormat.parse("$day $month $year")
+        } catch (e: ParseException) {
+            null
+        }
+
+        newDateFormat = SimpleDateFormat("d MMMM, EEEE", Locale.getDefault())
+
+        return if (date != null) newDateFormat.format(date) else "Такого дня не существует"
     }
 }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,36 @@
 package subtask3
 
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var sumInt = 0
+        val newString = StringBuilder()
+        var date = LocalDate.of(2000, 12, 30)
+        for (i in blockA) {
+
+            when (i) {
+                is Int -> sumInt += i
+                is String -> newString.append(i)
+                is LocalDate -> {
+                    blockA.forEach {
+                        if (it is LocalDate) {
+                            if (it > date) date = it
+                        }
+                    }
+
+                }
+            }
+        }
+
+        when(blockB){
+            String::class -> return newString.toString()
+            Int::class -> return sumInt
+            LocalDate::class -> return "${date.dayOfMonth}.${date.monthValue}.${date.year}"
+            else -> return 0
+        }
     }
 }
